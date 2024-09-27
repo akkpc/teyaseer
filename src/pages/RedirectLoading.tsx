cc083e7b85a1e2bf8c74f1d9261cd6abdfb4a652
akkpc <aanand@kissflow.com>
2024-09-27 16:31:04 +0530

    [Ak] Updated redirect loading page

--- src/pages/RedirectLoading.tsx
+++ src/pages/RedirectLoading.tsx
@@ -5,34 +5,21 @@ import { useSearchParams } from 'react-router-dom';
 export default function RedirectLoading() {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    // async function authenticate(code: string) {
-    //     const { REACT_APP_UAE_PASS_STG_URL } = process.env;
-    //     const authUrl = `${REACT_APP_UAE_PASS_STG_URL}/idshub/token`
-    //     const bodyParams = {
-    //         'grant_type': 'authorization_code',
-    //         // 'redirect_uri': 'https://uaepass-60eaa.web.app',
-    //         'code': code,
+    async function authenticate(code: string) {
+        const { REACT_APP_API_URL } = process.env;
+        const authUrl = `${REACT_APP_API_URL}/api/v1/auth/${code}`
 
-    //     }
-    //     const headers = {
-    //         'Authorization': btoa(`sandbox_stage:sandbox_stage`),
-    //         'Content-Type': 'application/x-www-form-urlencoded'
-    //     }
-
-    //     const response = await axios.post(authUrl, {
-    //         headers,
-    //         body: JSON.stringify(bodyParams)
-    //     }).catch((err) => {
-    //         console.log("Failed to fetch from staging url")
-    //     })
-    //     console.log("Response :", response)
-    // }
+        const response = await axios.get(authUrl).catch((err) => {
+            console.log("Failed to fetch from staging url" + err)
+        })
+        console.log("Response:  " , response)
+    }
     useEffect(() => {
         const authcode: string | null = searchParams.get("code");
         if (authcode) {
-            // (async () => {
-            //     await authenticate(authcode);
-            // })()
+            (async () => {
+                await authenticate(authcode);
+            })()
         }
     }, [])
     return (
