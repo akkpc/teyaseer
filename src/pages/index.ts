aab8b7d81f52c3528d0e51c4712e4faf1876fc46
akkpc <aanand@kissflow.com>
2024-09-27 12:44:25 +0530

    Add missed file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ import { Home } from "./Home"
 import RedirectLoading from "./RedirectLoading"
 import SignIn from "./Signin"
 import VendorRegistration from "./VendorRegistration"
-import Register from "./Registeration"
+import Register from "./Registration"
 export {
     Home, RedirectLoading, SignIn, VendorRegistration, Register as ContractLicensePage
 }
